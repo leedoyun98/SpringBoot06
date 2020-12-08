afecbeb013330bd8a7fb03a00a5d0d8b089f52af
leedo <leedoyun98@naver.com>
2020-12-08 21:43:28 +0900

    f

---
+++ src/main/java/com/example/demo/services/ArticleService.java
@@ -0,0 +1,15 @@
+package com.example.demo.services;
+
+import com.example.demo.domains.ArticleDto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ArticleService {
+
+	public int write(ArticleDto articleDto);
+
+	
+    
+ 
+}
